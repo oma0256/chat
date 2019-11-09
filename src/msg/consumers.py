a15a2eb4da38aca871f8d4dc64fa4329571fa078
Jonathan Omarwoth <oma0256@gmail.com>
2019-11-09 17:43:21 +0300

    Persist messages to database

--- src/msg/consumers.py
+++ src/msg/consumers.py
@@ -1,15 +1,18 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from .models import Thread
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import Thread, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        reciever = self.scope['url_route']['kwargs']['username']
+        self.reciever = self.scope['url_route']['kwargs']['username']
         self.sender = self.scope['user']
-        thread = await self.get_thread(self.sender, reciever)
-        self.room_group_name = f'chat_{thread.id}'
+        self.thread = await self.get_thread(self.sender.username,
+                                            self.reciever)
+        self.room_group_name = f'chat_{self.thread.id}'
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -38,6 +41,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def chat_message(self, event):
         message = event['message']
+        reciever = await self.get_user_by_username(self.reciever)
+        await self.create_message(self.thread, self.sender, reciever, message)
         await self.send(json.dumps({
             'message': message,
             'sender': self.sender.username
@@ -46,3 +51,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
     @database_sync_to_async
     def get_thread(self, sender, reciever):
         return Thread.objects.get_thread_or_create(sender, reciever)
+
+    @database_sync_to_async
+    def get_user_by_username(self, username):
+        return get_object_or_404(User, username=username)
+
+    @database_sync_to_async
+    def create_message(self, thread, sender, reciever, message):
+        return Message.objects.create(thread=thread, sender=sender,
+                                      reciever=reciever, message=message)
